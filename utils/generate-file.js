a489b1988a73e6a48165a19bdda3cf4dcdb54265
Charlie H <charltonhua@gmail.com>
2022-01-06 19:31:46 -0800

    update app.js

--- utils/generate-file.js
+++ utils/generate-file.js
@@ -20,4 +20,22 @@ const writeFile = (fileContent) => {
   });
 };
 
-module.exports = { writeFile };
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: "Style sheet copied successfully!",
+      });
+    });
+  });
+};
+
+module.exports = {
+  writeFile: writeFile,
+  copyFile: copyFile,
+};
